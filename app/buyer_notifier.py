5831f57b8961455b22a4f48a18b8d02f9e799684
vortexai2026 <juanclaud8001@gmail.com>
2026-02-10 18:54:24 -0600

    Create buyer_notifier.py

---
+++ app/buyer_notifier.py
@@ -0,0 +1,9 @@
+# app/buyer_notifier.py
+
+def notify_buyers_for_deal(deal: dict):
+    """
+    Temporary notifier.
+    Later this will send email / SMS / dashboard alerts.
+    """
+    print("🔔 Notifying buyers for deal:")
+    print(deal)

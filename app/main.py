90f5bd64cb4845f1b2a12d5b864b512cee106b27
vortexai2026 <juanclaud8001@gmail.com>
2026-02-10 19:07:35 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.deals_routes import router as deals_router
 from app.outreach_routes import router as outreach_router
-# from app.buyers_routes import router as buyers_router  # TEMP OFF
+# from app.buyers_routes import router as buyers_router  # TEMP DISABLED
 
 app = FastAPI(title="VortexAI Backend", version="1.0")
 
@@ -15,4 +15,4 @@ def health():
 
 app.include_router(deals_router)
 app.include_router(outreach_router)
-# app.include_router(buyers_router)  # TEMP OFF
+# app.include_router(buyers_router)  # TEMP DISABLED

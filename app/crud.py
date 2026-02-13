38b160b34023498947331d261c4d75a50999cbfc
vortexai2026 <juanclaud8001@gmail.com>
2026-02-13 08:04:31 -0600

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -3,11 +3,11 @@ from sqlalchemy.future import select
 from .models import User
 
 async def create_user(db: AsyncSession, name: str, email: str):
-    user = User(name=name, email=email)
-    db.add(user)
+    new_user = User(name=name, email=email)
+    db.add(new_user)
     await db.commit()
-    await db.refresh(user)
-    return user
+    await db.refresh(new_user)
+    return new_user
 
 async def get_users(db: AsyncSession):
     result = await db.execute(select(User))

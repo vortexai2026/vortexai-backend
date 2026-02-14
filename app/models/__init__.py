de928f41f8028c54fe801f81e3b3a5979697503b
vortexai2026 <juanclaud8001@gmail.com>
2026-02-13 22:31:31 -0600

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,1 @@
-from fastapi import APIRouter
-from .buyers import router as buyers_router
-from .billing import router as billing_router
-
-router = APIRouter()
-router.include_router(buyers_router)
-router.include_router(billing_router)
-
+  

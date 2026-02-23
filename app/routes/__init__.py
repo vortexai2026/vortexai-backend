eca39c5910f60bf6d8aa2cb45309fc2511869882
vortexai2026 <juanclaud8001@gmail.com>
2026-02-22 18:44:44 -0600

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,5 @@
 # app/routes/__init__.py
 from .deals import router as deals_router
-from .buyers import router as buyers_router
 from .autonomous import router as autonomous_router
+
+__all__ = ["deals_router", "autonomous_router"]
